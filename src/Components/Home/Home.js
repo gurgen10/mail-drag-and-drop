117b7596e0a2a5cdbddc5adcf496ffa0a080fa71
gurgen10 <gurgentorosyan88@gmail.com>
2020-01-04 00:55:18 +0400

    added home text

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Pre = styled.p`
+font-size: 30px;
+color: green;
+width: 50%;
+margin: 0 auto;
+`;
 
 
 export default class Home extends Component {
@@ -6,7 +14,11 @@ export default class Home extends Component {
         return (
             <div>
                 <h1>Home</h1>
-                
+                <Pre>
+                    Hi dear user.
+                    You can reorder your mail list just dragging and dropping them and 
+                    you can move mail items into another mail boxes by draging and dropping mail items on specific box.
+                </Pre>
             </div>
         )
     }
